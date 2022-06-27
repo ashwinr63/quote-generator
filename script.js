2cc36b179e84c7e377d8b1749496220d4257f7b8
Ashwin <8831182+ashwinr63@users.noreply.github.com>
2022-06-26 20:30:53 -0400

    Adding eventListener, class and twitter intent

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const quoteContainer = document.getElementById('quote-container')
+const quoteText = document.getElementById('quote')
+const authorText = document.getElementById('author')
+const twitterBtn = document.getElementById('twitter')
+const newQuoteBtn = document.getElementById('new-quote')
+
+
 // APi quotes global Variables
 let apiQuotes = []
 
@@ -6,7 +13,22 @@ function newQuote() {
 
     // Picking a new Random Quote from Api
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
-    console.log(quote)
+    
+    // Check if Author is blank and replace it with "Unknown"
+    if (!quote.author) {
+        authorText.textContent = 'Unknown'
+    } else {
+        authorText.textContent = quote.author;
+    }
+
+    // check quote length to mention the style of the code. 
+    if (quote.text.length > 45) {
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.remove('long-quote')
+    }
+
+    quoteText.textContent = quote.text;
 }
 
 // Get quotes from API
@@ -22,8 +44,19 @@ async function getQuotes() {
     catch (error) {
         //catch Error Exception here
     }
-
 }
+
+// Twitter Quote
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank')
+}
+
+// EventListeners 
+
+//newQuote Listener
+newQuoteBtn.addEventListener('click', newQuote)
+twitterBtn.addEventListener('click', tweetQuote)
 
    //on Load function
    getQuotes();
